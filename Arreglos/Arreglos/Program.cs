094146eac18763320271a50730f82c7172fecb31
emivnajera <emivelasquez2000@hotmail.com>
2022-05-16 14:57:04 -0600

    se agrega archivo de arrays

---
+++ Arreglos/Arreglos/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string[] tiposCafe;
+float[] valores;
+
+tiposCafe = new string[] { "Expreso", "Largo", "Filtrado", "Latte" };
+valores = new float[] { 1.2f, 1.5f, 5.0f, 5.5f };
+
+for(int i = 0; i < tiposCafe.Length; i++)
+{
+    Console.WriteLine(tiposCafe[i] + ": " + valores[i]);
+}
---
+++ LeerDatosUsuario/LeerDatosUsuario/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeerDatosUsuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el lado a");
+            float sideA =float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa el lado b");
+            float sideB =float.Parse(Console.ReadLine());
+
+            float area = sideA * sideB;
+
+            Console.WriteLine(area);
+           
+        }
+    }
+}
