d1ce37c3d8fc895fa2aea862a15766a1452264c8
emivnajera <emivelasquez2000@hotmail.com>
2022-05-16 14:44:11 -0600

    se agrega archivo de archivos relacionales.

---
+++ OperadoresRelacionales/OperadoresRelacionales/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int value1 = 3;
+int value2 = 18;
+
+bool result = value1 == value2;
+Console.WriteLine(result);
+
+result = value1 != value2;
+Console.WriteLine(result);
+
+result = value1 > value2;
+Console.WriteLine(result);
+
+result = value1 < value2;
+Console.WriteLine(result);
+
+result = value1 >= value2;
+Console.WriteLine(result);
+
+result = value1 <= value2;
+Console.WriteLine(result);
+
+
