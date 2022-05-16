b2e1737f44eb12830c301489153923483e225628
emivnajera <emivelasquez2000@hotmail.com>
2022-05-16 15:05:32 -0600

    se agrega archivo de listas.

---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> listadeCompras = new List<string>();
+
+
+listadeCompras.Add("Leche");
+listadeCompras.Add("Huevos");
+listadeCompras.Add("Carne");
+listadeCompras.Add("Coca-Cola");
+
+
+for(int i = 0; i < listadeCompras.Count; i++)
+{
+    Console.WriteLine(listadeCompras[i]);
+}
+listadeCompras.Remove("Carne");
+
+Console.WriteLine();
+
+for (int i = 0; i < listadeCompras.Count; i++)
+{
+    Console.WriteLine(listadeCompras[i]);
+}
