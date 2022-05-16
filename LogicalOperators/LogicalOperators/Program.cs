6c527ddb1db550bf6dbd47fb8ace5c31fba6a063
emivnajera <emivelasquez2000@hotmail.com>
2022-05-15 21:23:07 -0600

    se agrego archivo de operadores logicos.

---
+++ LogicalOperators/LogicalOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+bool value1 = true;
+bool value2 = false;
+
+bool result = value1 && value2;
+Console.WriteLine(result);
+
+
+result = value1 || value2;
+Console.WriteLine(result);
+
+
+
+result = !value1;
+Console.WriteLine(result);
